e7b579bf29ce55479ce23ce624e953bd3fea98d3
seniorvladislav <vmelikyan161@gmail.com>
2021-05-18 13:31:15 +0300

    without resp

--- handlers/tiktok.js
+++ handlers/tiktok.js
@@ -70,7 +70,7 @@ const getVideo = async url => {
 
     const { body, statusCode } = await post(options);
 
-    return console.log(resp.statusCode, resp.body);
+    return console.log(statusCode, body);
 
     if (statusCode === 200) {
       // return console.log(resp.body);
